3390c5ad5599df5170f7a901be10a02996e945b4
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-05-27 18:54:03 +0700

    void complete

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -15,6 +15,8 @@ class pasien{
             cout << "pasien \"" << nama 
             << "\" tidak ada \n";
         }
+        void tambahDokter(dokter*);
+        void cetakDokter();
 };
 
 class dokter{
@@ -30,4 +32,19 @@ class dokter{
             cout << "dokter \"" << nama 
             << "\" tidak ada \n";
         }
+        void tambahPasien(pasien*);
+        void cetakPasien();
 };
+
+void pasien :: tambahDokter (dokter* pDokter){
+    daftar_dokter.push_back(pDokter);
+}
+
+void pasien::cetakDokter(){
+    cout << "daftar dokter yang menangani pasien \""
+    << this->nama << "\":\n";
+    for (auto& a : daftar_dokter){
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
