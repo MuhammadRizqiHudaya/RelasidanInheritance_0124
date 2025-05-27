a2fbfa3544f90b546e87e26ec53ff282b0debe5f
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-05-27 19:02:47 +0700

    pointer

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -58,4 +58,29 @@ void dokter::cetakPasien(){
     cout << endl;
 }
 
-int main()
+int main(){
+    dokter* varDokter1 = new dokter("dr.Budi");
+    dokter* varDokter2 = new dokter("dr.Budi");
+    pasien* varPasien1 = new pasien("dr.Budi");
+    pasien* varPasien2 = new pasien("dr.Budi");
+
+    varDokter1->tambahPasien(varPasien1);
+    varDokter1->tambahPasien(varPasien2);
+    varDokter2->tambahPasien(varPasien1);
+    
+    varPasien1->tambahDokter(varDokter1);
+    varPasien2->tambahDokter(varDokter1);
+    varPasien1->tambahDokter(varDokter2);
+
+    varDokter1->cetakPasien();
+    varDokter2->cetakPasien();
+    varPasien1->cetakDokter();
+    varPasien2->cetakDokter();
+
+    delete varPasien1;
+    delete varPasien2;
+    delete varDokter1;
+    delete varDokter2;
+
+    return 0;
+}
