8937e2524c2d86da99b5516c93b6ad2e955fa918
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-05-27 18:22:19 +0700

    void update

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -13,6 +13,18 @@ class ibu{
         ~ibu() {
             cout << "ibu\"" << nama << "\" tidak ada\n";
         }
-
-        
+        void tambahAnak (anak*);
+        void cetakAnak();
+};
+void ibu::tambahAnak(anak* pAnak){
+    daftar_anak.push_back(pAnak);
 }
+void ibu::cetakAnak(){
+    cout << "Daftar anak dari ibu \"" << 
+    this->nama << "\":\n";
+    for (int i = 0; i < daftar_anak.size(); i++){
+        cout << daftar_anak[i]->nama << endl;
+    }
+    cout << endl;
+}
+#endif
