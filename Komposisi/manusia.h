b4f85a9015e137051dbe4902e212ad3f59883a46
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-05-27 19:38:33 +0700

    complete

--- Komposisi/manusia.h
+++ Komposisi/manusia.h
@@ -1,15 +1,15 @@
 #ifndef MANUSIA_H
 #define MANUSIA_H
+using namespace std;
 
 class manusia {
+public:
     string name;
     jantung varJantung;
 
     manusia(string pName)
-    : name(pName), varJantung() {
-        cout << "Manusia \"" << name << "\" ada\n";
-    }: name(pName){
-        cout << name << "hidup\n";
+    : name(pName) {
+        cout <<  name << "hidup\n";
     }
     ~manusia(){
         cout << name << "mati\n";
