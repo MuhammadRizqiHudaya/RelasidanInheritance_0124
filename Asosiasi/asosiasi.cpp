6d59079f67d153c2c98cdf8e0e679eb542206c04
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-05-27 18:56:24 +0700

    real complete void

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -48,3 +48,14 @@ void pasien::cetakDokter(){
     }
     cout << endl;
 }
+
+void dokter::cetakPasien(){
+    cout << "daftar pasien dari dokter \""
+    << this->nama << "\":\n";
+    for (auto& a : daftar_pasien){
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+
+int main()
